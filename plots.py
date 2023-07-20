de6a0201209a718969e2a8ab69dd32071d6308ca
Jinseo Park <truewis@uchicago.edu>
2023-07-19 22:54:44 -0500

    event with uproot ttree

---
+++ plots.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import Event
+events = []
+if __name__ == "__main__":
+    pass
+
+def mass_spectrum_plot():
+    pass
+def angle_distribution_plot():
+    pass
+def angle_mass_plot():
+    pass
+def event_rate():
+    pass
+

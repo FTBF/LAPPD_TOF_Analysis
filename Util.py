62fa8ca2e55f34e57cd01d5bb2b2a891641adced
Jinseo Park <cloxionnt@gmail.com>
2024-06-27 14:43:27 -0500

    Update Util.py

--- Util.py
+++ Util.py
@@ -1285,7 +1285,7 @@ class Util:
 		#channels = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29])
 		for event in events:
 			xdata = np.linspace(0, 255,256)*25/256
-			ydata = np.linspace(-0.5, 29.5, 31)
+			ydata = np.linspace(0, 29, 30)
 			zdata = sineData[event,channels,:]
 			plt.clf()
 			fig, ax1 = plt.subplots(1, 1)
